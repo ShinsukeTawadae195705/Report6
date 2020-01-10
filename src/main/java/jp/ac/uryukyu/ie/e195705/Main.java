0f76420f5b89e06836973fb712313d30df111e61
Shinsuke Tawada <e195705@ie.u-ryukyu.ac.jp>
2020-01-10 14:44:28 +0900

    カ行

--- src/main/java/jp/ac/uryukyu/ie/e195705/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195705/Main.java
@@ -1,4 +1,16 @@
 package jp.ac.uryukyu.ie.e195705;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args){
+    }
+}
+
+class word{
+    List<String> words_A = new ArrayList<>(Arrays.asList("アルキメデス","インテグラル","ウラ","エピサイクロイド","オイラー"));
+    List<String> words_K = new ArrayList<>(Arrays.asList("カクリツブンプ","キョスウ","クラインノツボ","ケーニヒスベルグ","コセカント"));
+    
 }
