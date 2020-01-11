5864ea765a9036f804c667e6d2b9bad82f68d010
Shinsuke Tawada <e195705@ie.u-ryukyu.ac.jp>
2020-01-11 14:29:16 +0900

    ラ行

--- src/main/java/jp/ac/uryukyu/ie/e195705/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195705/Main.java
@@ -15,8 +15,8 @@ class word{
     List<String> words_S = new ArrayList<>(Arrays.asList("サイクロイド","シゼンタイス（自然対数）ウ","スーパーソス（スーパー素数）ウ","セイキブン（正規分布）プ","ソインスウブンカ（素因数分解）イ"));
     List<String> words_T = new ArrayList<>(Arrays.asList("タンジェント","チャンパーノウンテイス（チャンパーノウン定数）ウ","ツェラーノコウシ（ツェラーの公式）キ","テイラーテンカ（テイラー展開）イ","トクセイホウテイシ（特性方程式）キ"));
     List<String> words_N = new ArrayList<>(Arrays.asList("ナン（NAND）ド","ニコウケイス（二項係数）ウ","","ネイピアス（ネイピア数）ウ","ノウ（濃度）ド"));
-    List<String> words_H = new ArrayList<>(Arrays.asList(""));
-    List<String> words_M = new ArrayList<>(Arrays.asList());
-    List<String> words_Y = new ArrayList<>(Arrays.asList());
-    List<String> words_R = new ArrayList<>(Arrays.asList());
+    List<String> words_H = new ArrayList<>(Arrays.asList("ハミルトンサイクル","ヒルベルトホテル","フィボナッチスウレ（フィボナッチ数列）ツ","ヘロンノコウシ（ヘロンの公式）キ","ホシュウゴ（補集合）う"));
+    List<String> words_M = new ArrayList<>(Arrays.asList("マンデルブロシュウゴ（マンデルブロ集合）ウ","ミチス（未知数）ウ","ムゲンダ（無限大）イ","メルセンヌ","モンテカルロホ（モンテカルロ法）ウ"));
+    List<String> words_Y = new ArrayList<>(Arrays.asList("ヤコビギョウレ（ヤコビ行列）ツ","ユークリッド","ヨゲンテイ（余弦定理）リ"));
+    List<String> words_R = new ArrayList<>(Arrays.asList("ライプニッツ","リシンリ（離心率）ツ","ルーローノサンカッケ（ルーローの三角形）イ","レーダーチャート","ロンリエンザン（論理演算子）シ"));
 }
