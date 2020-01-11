25592b20d88aedcaa06d021322f6ac9995014694
Shinsuke Tawada <e195705@ie.u-ryukyu.ac.jp>
2020-01-11 14:05:25 +0900

    ナ行

--- src/main/java/jp/ac/uryukyu/ie/e195705/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195705/Main.java
@@ -10,7 +10,13 @@ public class Main {
 }
 
 class word{
-    List<String> words_A = new ArrayList<>(Arrays.asList("アルキメデス","インテグラル","ウラ","エピサイクロイド","オイラー"));
-    List<String> words_K = new ArrayList<>(Arrays.asList("カクリツブンプ","キョスウ","クラインノツボ","ケーニヒスベルグ","コセカント"));
-    
+    List<String> words_A = new ArrayList<>(Arrays.asList("アルキメデス","インテグラル","ウ（裏）ラ","エピサイクロイド","オイラー"));
+    List<String> words_K = new ArrayList<>(Arrays.asList("カクリツブン（確率分布）プ","キョス（虚数）ウ","クラインノツ（クラインの壺）ボ","ケーニヒスベルグ","コセカント"));
+    List<String> words_S = new ArrayList<>(Arrays.asList("サイクロイド","シゼンタイス（自然対数）ウ","スーパーソス（スーパー素数）ウ","セイキブン（正規分布）プ","ソインスウブンカ（素因数分解）イ"));
+    List<String> words_T = new ArrayList<>(Arrays.asList("タンジェント","チャンパーノウンテイス（チャンパーノウン定数）ウ","ツェラーノコウシ（ツェラーの公式）キ","テイラーテンカ（テイラー展開）イ","トクセイホウテイシ（特性方程式）キ"));
+    List<String> words_N = new ArrayList<>(Arrays.asList("ナン（NAND）ド","ニコウケイス（二項係数）ウ","","ネイピアス（ネイピア数）ウ","ノウ（濃度）ド"));
+    List<String> words_H = new ArrayList<>(Arrays.asList(""));
+    List<String> words_M = new ArrayList<>(Arrays.asList());
+    List<String> words_Y = new ArrayList<>(Arrays.asList());
+    List<String> words_R = new ArrayList<>(Arrays.asList());
 }
