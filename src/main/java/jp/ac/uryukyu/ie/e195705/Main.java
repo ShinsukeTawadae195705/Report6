41bf9b99f9f399a845eb4c768c6e7a133da72ce6
Shinsuke Tawada <e195705@ie.u-ryukyu.ac.jp>
2020-01-13 17:43:50 +0900

    word_endをPlayerクラスからComputerクラスに継承できなかたっため、新たなにActionクラスを作り、そこにまとめてしりとりの機能を任せる

--- src/main/java/jp/ac/uryukyu/ie/e195705/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195705/Main.java
@@ -9,13 +9,15 @@ abstract class Character{
     String name;
     String word_end;
     String word_initial;
-    Character(String name){
+
+    void Name(String name) {
         this.name = name;
     }
 }
 
-class words {
-    //効率よくしりとりを行いたいため全てをカタカナで表記した。わかりにくい場合は()を用いて表記した。
+class Words {
+    //効率よくしりとりを行いたいため全てをカタカナで表記する。わかりにくい場合は()を用いて表記する。
+    List<String> words = new ArrayList<>();
     List<String> words_A = new ArrayList<>(Arrays.asList("アルキメデス", "インテグラル", "ウ（裏）ラ", "エピサイクロイド", "オイラー"));
     List<String> words_K = new ArrayList<>(Arrays.asList("カクリツブン（確率分布）プ", "キョス（虚数）ウ", "クラインノツ（クラインの壺）ボ", "ケーニヒスベルグ", "コセカント"));
     List<String> words_S = new ArrayList<>(Arrays.asList("サイクロイド", "シゼンタイス（自然対数）ウ", "スーパーソス（スーパー素数）ウ", "セイキブン（正規分布）プ", "ソインスウブンカ（素因数分解）イ"));
@@ -23,32 +25,56 @@ class words {
     List<String> words_N = new ArrayList<>(Arrays.asList("ナン（NAND）ド", "ニコウケイス（二項係数）ウ", "", "ネイピアス（ネイピア数）ウ", "ノウ（濃度）ド"));
     List<String> words_H = new ArrayList<>(Arrays.asList("ハミルトンサイクル", "ヒルベルトホテル", "フィボナッチスウレ（フィボナッチ数列）ツ", "ヘロンノコウシ（ヘロンの公式）キ", "ホシュウゴ（補集合）う"));
     List<String> words_M = new ArrayList<>(Arrays.asList("マンデルブロシュウゴ（マンデルブロ集合）ウ", "ミチス（未知数）ウ", "ムゲンダ（無限大）イ", "メルセンヌ", "モンテカルロホ（モンテカルロ法）ウ"));
-    List<String> words_Y = new ArrayList<>(Arrays.asList("ヤコビギョウレ（ヤコビ行列）ツ", "ユークリッド", "ヨゲンテイ（余弦定理）リ"));
+    List<String> words_Y = new ArrayList<>(Arrays.asList("ヤコビギョウレ（ヤコビ行列）ツ","", "ユークリッド","", "ヨゲンテイ（余弦定理）リ"));
     List<String> words_R = new ArrayList<>(Arrays.asList("ライプニッツ", "リシンリ（離心率）ツ", "ルーローノサンカッケ（ルーローの三角形）イ", "レーダーチャート", "ロンリエンザン（論理演算子）シ"));
+
+    void words(){
+        //javaにはタプルが存在しないため、1行で要素を追加した。（そうでないと行数を稼いでるようになってしまうので、、）
+        words.addAll(words_A);words.addAll(words_K);words.addAll(words_S);words.addAll(words_T);words.addAll(words_N);words.addAll(words_H);words.addAll(words_M);words.addAll(words_Y);words.addAll(words_R);
+    }
 }
 
 class Player extends Character{
-    Player(String name) {
-        super(name);
+    void Player_name(String name) {
+        Name(name);
     }
 
-    public void Action(){
-        int count = 1;
+    public void Action_P(){
         System.out.println(this.name+"のターン");
-        while(count > 0) {
+    }
+}
+
+class Computer extends Character{
+    void Computer_name(String name){
+        Name(name);
+    }
+
+
+    public void Action_C(){
+        System.out.println(this.name+"のターン");
+        Words word = new Words();
+        word.words();
+        System.out.println(word.words.indexOf(word_end));
+    }
+}
+
+class Action{
+    int count = 1;
+    String word_end;
+    String word_initial;
+    public void Action(){
+        while (count > 0){
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();//String型の文字列を入力したいからnextLine()を用いる
             word_end = input.substring(input.length() - 1);//入力した文字列の末尾1文字を取得する
             if (word_end.matches("[ア-ン]")) {//正規表現は""で括る
-                if (word_end.matches(("[ヌワヲ]"))) {
+                if (word_end.matches(("[ヌワヲ ]"))) {
                     System.out.println("そのような数学的単語は私には見つけられぬ、、\n貴様の勝ちだ");
-                }
-                if (word_end.matches(("[ン]"))) {
+                }if (word_end.matches(("[ン]"))) {
                     System.out.println("貴様はしりとりのルールも知らんのか！");
-                    System.out.println(this.name + "の負け");
-                } else {
-                    System.out.println(input);
-                }break;
+                }if (word_end.matches("[ーャュョ]")){
+                    System.out.println("あ、それはなしで、、");
+                }
             } else {
                 System.out.println("申し訳ないがカタカナ表記で頼む、、");
             }
@@ -56,26 +82,20 @@ class Player extends Character{
     }
 }
 
-class Computer extends Character{
-    Computer(String name){
-        super(name);
-    }
-
-    public void Action(){
-        System.out.println(this.name+"のターン");
-        word_initial = word_end;
-
-    }
-}
-
 public class Main {
     public static void main(String[] args){
+        String word_end;
+        String word_initial;
         System.out.println("しりとりスタート\nしりとりの「り」からスタート");
-        Player player = new Player("チャンレンジャー");
-        Computer com = new Computer("数学的しりとりマスター");
-        player.Action();
-        com.Action();
-
-
+        Player player = new Player();
+        player.Player_name("チャレンジャー");
+        Computer com = new Computer();
+        com.Computer_name("数学的しりとりマスター");
+        while (true) {
+            player.Action_P();
+            Action action = new Action();
+            action.Action();
+            com.Action_C();
+        }
     }
 }
